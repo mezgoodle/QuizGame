be5651da0c5d02305bcd14767bf19d53b90ee82f
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-11-13 11:44:57 +0200

    Overwrite system button "Back"

--- app/src/main/java/mezidia/mezgoodle/quizgame/GameLevels.java
+++ app/src/main/java/mezidia/mezgoodle/quizgame/GameLevels.java
@@ -1,8 +1,11 @@
 package mezidia.mezgoodle.quizgame;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -15,5 +18,30 @@ public class GameLevels extends AppCompatActivity {
 
         Window w = getWindow();
         w.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        Button button_back = (Button)findViewById(R.id.button_back);
+
+        button_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // command for back button
+                try {
+                    Intent intent = new Intent(GameLevels.this, MainActivity.class);
+                    startActivity(intent);finish();
+                } catch (Exception e) {
+
+                }
+            }
+        });
+    }
+    // System button "Back"
+    @Override
+    public void onBackPressed() {
+        try {
+            Intent intent = new Intent(GameLevels.this, MainActivity.class);
+            startActivity(intent);finish();
+        } catch (Exception e) {
+
+        }
     }
 }
