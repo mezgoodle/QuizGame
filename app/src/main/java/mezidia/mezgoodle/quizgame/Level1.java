4e003f7610969edeab0d65299733c23591034655
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-11-15 12:22:04 +0200

    Set clip to outline

--- app/src/main/java/mezidia/mezgoodle/quizgame/Level1.java
+++ app/src/main/java/mezidia/mezgoodle/quizgame/Level1.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.ImageView;
 
 public class Level1 extends AppCompatActivity {
 
@@ -12,6 +13,13 @@ public class Level1 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.universal);
+
+        final ImageView img_left = (ImageView)findViewById(R.id.img_left);
+        // create round corners for left image
+        img_left.setClipToOutline(true);
+        final ImageView img_right = (ImageView)findViewById(R.id.img_right);
+        // create round corners for right image
+        img_right.setClipToOutline(true);
 
         // Show game on fullscreen
         Window w = getWindow();
