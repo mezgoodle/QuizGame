9f0eed487ee557af2fe7fc362a0dd1217b23bba6
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-11-18 09:16:19 +0200

    Create dialog window

--- app/src/main/java/mezidia/mezgoodle/quizgame/Level1.java
+++ app/src/main/java/mezidia/mezgoodle/quizgame/Level1.java
@@ -2,6 +2,9 @@ package mezidia.mezgoodle.quizgame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
@@ -9,20 +12,30 @@ import android.widget.ImageView;
 
 public class Level1 extends AppCompatActivity {
 
+    Dialog dialog;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.universal);
 
         final ImageView img_left = (ImageView)findViewById(R.id.img_left);
-        // create round corners for left image
+        // Create round corners for left image
         img_left.setClipToOutline(true);
         final ImageView img_right = (ImageView)findViewById(R.id.img_right);
-        // create round corners for right image
+        // Create round corners for right image
         img_right.setClipToOutline(true);
 
         // Show game on fullscreen
         Window w = getWindow();
         w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+
+        // Call dialog window on the start of game
+        dialog = new Dialog(this); // create new dialog window
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE); // hide window title
+        dialog.setContentView(R.layout.previewdialog); // path to layout of dialog window
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT)); // transparent color of dialog window
+        dialog.setCancelable(false); // cant close window with back button
+        dialog.show(); // show dialog window
     }
 }
