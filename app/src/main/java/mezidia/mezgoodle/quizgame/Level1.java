bcc3f419bf3ca83e4d1034e0d9acbb7f382dae93
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-11-18 09:27:37 +0200

    Dialog buttons

--- app/src/main/java/mezidia/mezgoodle/quizgame/Level1.java
+++ app/src/main/java/mezidia/mezgoodle/quizgame/Level1.java
@@ -3,12 +3,16 @@ package mezidia.mezgoodle.quizgame;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Dialog;
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class Level1 extends AppCompatActivity {
 
@@ -36,6 +40,31 @@ public class Level1 extends AppCompatActivity {
         dialog.setContentView(R.layout.previewdialog); // path to layout of dialog window
         dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT)); // transparent color of dialog window
         dialog.setCancelable(false); // cant close window with back button
+
+        // Button for closing dialog window
+        TextView btnclose = (TextView)dialog.findViewById(R.id.btnclose);
+        btnclose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    Intent intent = new Intent(Level1.this, GameLevels.class);
+                    startActivity(intent);finish();
+                } catch (Exception e) {
+                    // Empty
+                }
+                dialog.dismiss(); // Close dialog window
+            }
+        });
+
+        // Button for continue the activity
+        Button btncontinue = (Button)dialog.findViewById(R.id.btncontinue);
+        btncontinue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+
         dialog.show(); // show dialog window
     }
 }
