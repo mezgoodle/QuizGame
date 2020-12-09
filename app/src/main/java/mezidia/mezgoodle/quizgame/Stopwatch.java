5bfd5b351f1e5e8f680c233dc805ca07764ce74f
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-12-09 15:44:20 +0200

    Update Stopwatch.java

--- app/src/main/java/mezidia/mezgoodle/quizgame/Stopwatch.java
+++ app/src/main/java/mezidia/mezgoodle/quizgame/Stopwatch.java
@@ -1,7 +1,21 @@
 package mezidia.mezgoodle.quizgame;
 
+import android.annotation.SuppressLint;
+
 public class Stopwatch {
+    final int nanoseconds = 1000000000;
+    final int seconds = 60;
+
     public long GetTime() {
-        return System.nanoTime() / 1000000000;
+        return System.nanoTime() / this.nanoseconds;
+    }
+
+    @SuppressLint("DefaultLocale")
+    public String GetResult(long time) {
+        int minutes = (int) (time / this.seconds);
+        int seconds = (int) (time - this.seconds * minutes);
+        if (minutes == 0) {
+            return String.format("Your time is %d seconds", time);
+        } else return String.format("Your time is %d minutes and %d seconds.", minutes, seconds);
     }
 }
